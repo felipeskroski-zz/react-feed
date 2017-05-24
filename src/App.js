7c99c0cb3244d3fb05bb74340061459da198accf
felipeskroski <felipeskroski@gmail.com>
2017-05-24 14:17:55 +1200

    feat: better loading view

--- src/App.js
+++ src/App.js
@@ -97,7 +97,24 @@ const App = observer(class App extends Component {
     return(
       <div className="App">
         <div className="newsfeed">
-          <h2>Loading App ...</h2>
+          <div className="loading">
+            <div className="loader" title="2">
+              <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg"  x="0px" y="0px"
+                 width="50px" height="50px" viewBox="0 0 50 50" >
+              <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+                <animateTransform attributeType="xml"
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 25 25"
+                  to="360 25 25"
+                  dur="0.6s"
+                  repeatCount="indefinite"/>
+                </path>
+              </svg>
+            </div>
+            <h2>Loading React Feed</h2>
+          </div>
+
         </div>
       </div>
     )
